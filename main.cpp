1e5f81bbbd0d74ee05ebd57ac8e5d577e770d52e
iw46teamcn <123747953+iw46teamcn@users.noreply.github.com>
2023-06-16 10:56:57 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<windows.h>
+#include<iostream>
+#include<stdlib.h>
+#pragma comment(lib,"winmm.lib")
+#include<mmsystem.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	
+	
+	MessageBox(0,"IwMusic","V1.0011",MB_OK);
+	
+	MessageBox(0,"This is a pre-realese","V1.0011",MB_OK);
+	
+	int a;
+	
+	MessageBox(0,"Buy Password at ours Github","Warn!",MB_OK | MB_ICONERROR);
+	system("start https://github.com/iw46teamcn/IwMusic");
+	
+	cin>>a;
+	if(a==1){
+	
+		while(1){
+		
+		
+			MessageBox(0,"By iw46 and LYMwyh","Message",MB_RETRYCANCEL | MB_ICONASTERISK );
+			cout<<"Some Message HAhaHA"<<endl;
+		
+		
+		}
+	}	
+	return 0;
+} 
